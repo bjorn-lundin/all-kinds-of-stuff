3358b899e2c1cfc593fc2520deb1b1bf0bc949f2
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2015-04-27 16:44:32 +0000

    newstructure

---
+++ brickpi_ada/create_constants.adb
@@ -0,0 +1,8 @@
+
+with Brickpi.Thin;
+
+procedure Create_Constants is
+begin
+  Brickpi.Thin.Print_Constants;
+end Create_Constants;
+
--- brickpi_ada/motor_test.adb
+++ brickpi_ada/motor_test.adb
@@ -1,16 +1,17 @@
 
 
 with Interfaces.C; use Interfaces.C;
-with Brickpi;
+with Brickpi.Thin;
+--with Brickpi.Constants;
 with Text_Io; use Text_Io;
 
 procedure Motor_Test is
   V,F,R : Int := -2;
-  Brick : Brickpi.Brick_Pi_Record_Pointer := Brickpi.Get_Pointer_To_Brick_Pi;
+  Brick : Brickpi.Thin.Brick_Pi_Record_Pointer := Brickpi.Thin.Get_Pointer_To_Brick_Pi;
 begin
 
-  Brickpi.Clear_Tick;
-  R := Brickpi.Setup;
+  Brickpi.Thin.Clear_Tick;
+  R := Brickpi.Thin.Setup;
   Put_Line("Setup result " & R'Img);
   if R > 0 then 
     return;
@@ -19,9 +20,9 @@ begin
   Brick.Address(1) := 1;
   Brick.Address(2) := 2;
 
-  Brick.Motor_Enable(Brickpi.PORT_A) := 1;
-  Brick.Motor_Enable(Brickpi.PORT_B) := 1;
-  R := Brickpi.Setup_Sensors;
+  Brick.Motor_Enable(Brickpi.Thin.PORT_A) := 1;
+  Brick.Motor_Enable(Brickpi.Thin.PORT_B) := 1;
+  R := Brickpi.Thin.Setup_Sensors;
   Put_Line("Setup_Sensors result " & R'Img);
   if R > 0 then 
     return;
@@ -29,16 +30,16 @@ begin
 
   loop
     if F = 1  then
-      Brick.Motor_Speed(BrickPi.PORT_A):= 80;
-      Brick.Motor_Speed(BrickPi.PORT_B):= 100;
+      Brick.Motor_Speed(BrickPi.Thin.PORT_A):= 80;
+      Brick.Motor_Speed(BrickPi.Thin.PORT_B):= 100;
       if v > 30 then
         F := 0;
         V := 0;
       end if;
       v := v+1;
     else
-      Brick.Motor_Speed(BrickPi.PORT_A):= -80;
-      Brick.Motor_Speed(BrickPi.PORT_B):= -100;
+      Brick.Motor_Speed(BrickPi.Thin.PORT_A):= -80;
+      Brick.Motor_Speed(BrickPi.Thin.PORT_B):= -100;
       if v > 30 then
         F := 1;
         V := 0;
@@ -46,17 +47,17 @@ begin
       v := v+1;      
     end if;
     
-    R := BrickPi.Update_Values;
+    R := Brickpi.Thin.Update_Values;
     Put_Line("Update_Values result " & R'Img);
     if R > 0 then 
       return;
     end if;
     
-    Put_Line("Encoder A " & Brick.Encoder(BrickPi.PORT_A)'Img);
-    Put_Line("Encoder B " & Brick.Encoder(BrickPi.PORT_B)'Img);
+    Put_Line("Encoder A " & Brick.Encoder(BrickPi.Thin.PORT_A)'Img);
+    Put_Line("Encoder B " & Brick.Encoder(BrickPi.Thin.PORT_B)'Img);
 
-    Put_Line("Encoder_Offset A " & Brick.Encoder_Offset(BrickPi.PORT_A)'Img);
-    Put_Line("Encoder_Offset B " & Brick.Encoder_Offset(BrickPi.PORT_B)'Img);
+    Put_Line("Encoder_Offset A " & Brick.Encoder_Offset(BrickPi.Thin.PORT_A)'Img);
+    Put_Line("Encoder_Offset B " & Brick.Encoder_Offset(BrickPi.Thin.PORT_B)'Img);
     
     delay 0.1;
   
