4f9ce3c91be34e47a424f18454496d880ce90199
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2017-01-19 17:08:32 +0000

    joymovement detected if video initiated

--- trunk/sdlada-1/sdl/telescope/tele.adb
+++ trunk/sdlada-1/sdl/telescope/tele.adb
@@ -26,13 +26,13 @@ procedure Tele is
     --Put_Line(Calendar2.Clock.To_String & " " & Who & " " & What);
     Put_Line( Who & " " & What);
   end Log;
-  
-  
+
+
 
   procedure Handle_Events(Quit : out Boolean) is
    Event : SDL.Events.Event;
-   Pollevent_Result : C.Int;  
-  begin 
+   Pollevent_Result : C.Int;
+  begin
     Quit := False;
   --  Log("Handle_Events","Start");
     loop
@@ -55,9 +55,9 @@ procedure Tele is
              elsif Name_Of_Key = "s" then null;
              elsif Name_Of_Key = "z" then null;
              elsif Name_Of_Key = "a" then null;
-             end if;             
+             end if;
            end;
-           
+
          when SDL.Events.KEYUP =>
            null;
            declare
@@ -68,50 +68,50 @@ procedure Tele is
              Log("Handle_Events"," Key up: '" & Name_Of_Key & "'");
              if Name_Of_Key = "escape" then
                Quit := True;
-             end if;             
+             end if;
            end;
-           
+
          when SDL.Events.QUIT =>
            Log("Handle_Events"," QUIT Event.The_Type " & Event.The_Type'Img );
            Quit := True;
-           
-         when SDL.Events.MOUSEBUTTONDOWN =>         
+
+         when SDL.Events.MOUSEBUTTONDOWN =>
            Log("Handle_Events"," MOUSEBUTTONDOWN x,y:" & Event.Button.X'Img & "," & Event.Button.Y'Img);
 
          when SDL.Events.JOYAXISMOTION =>
-           Log("Handle_Events"," JOYAXISMOTION Which,Axis,Value:" & Event.Jaxis.Which'Img & "," & 
-                                                                       Event.Jaxis.Axis'Img & "," & 
-                                                                       Event.Jaxis.Value'Img ); 
+           Log("Handle_Events"," JOYAXISMOTION Which,Axis,Value:" & Event.Jaxis.Which'Img & "," &
+                                                                       Event.Jaxis.Axis'Img & "," &
+                                                                       Event.Jaxis.Value'Img );
          when SDL.Events.JOYBALLMOTION =>
-           Log("Handle_Events"," JOYBALLMOTION Which,Ball,Xrel,Yrel:" & Event.Jball.Which'Img & "," & 
-                                                                       Event.Jball.Ball'Img & "," & 
-                                                                       Event.Jball.Xrel'Img & "," & 
-                                                                       Event.Jball.Yrel'Img ); 
+           Log("Handle_Events"," JOYBALLMOTION Which,Ball,Xrel,Yrel:" & Event.Jball.Which'Img & "," &
+                                                                       Event.Jball.Ball'Img & "," &
+                                                                       Event.Jball.Xrel'Img & "," &
+                                                                       Event.Jball.Yrel'Img );
 
          when SDL.Events.JOYBUTTONDOWN =>
-           Log("Handle_Events"," JOYBUTTONDOWN Which,Button,State:" & Event.Jbutton.Which'Img & "," & 
-                                                                       Event.Jbutton.Button'Img & "," & 
-                                                                       Event.Jbutton.State'Img ); 
-                                                                       
+           Log("Handle_Events"," JOYBUTTONDOWN Which,Button,State:" & Event.Jbutton.Which'Img & "," &
+                                                                       Event.Jbutton.Button'Img & "," &
+                                                                       Event.Jbutton.State'Img );
+
          when SDL.Events.JOYBUTTONUP =>
-           Log("Handle_Events"," JOYBUTTONUP Which,Button,State:" & Event.Jbutton.Which'Img & "," & 
-                                                                       Event.Jbutton.Button'Img & "," & 
-                                                                       Event.Jbutton.State'Img ); 
+           Log("Handle_Events"," JOYBUTTONUP Which,Button,State:" & Event.Jbutton.Which'Img & "," &
+                                                                       Event.Jbutton.Button'Img & "," &
+                                                                       Event.Jbutton.State'Img );
 
          when SDL.Events.JOYHATMOTION =>
-           Log("Handle_Events"," JOYHATMOTION Which,Hat,Value:" & Event.Jhat.Which'Img & "," & 
-                                                                       Event.Jhat.Hat'Img & "," & 
-                                                                       Event.Jhat.Value'Img); 
-                                                                       
+           Log("Handle_Events"," JOYHATMOTION Which,Hat,Value:" & Event.Jhat.Which'Img & "," &
+                                                                       Event.Jhat.Hat'Img & "," &
+                                                                       Event.Jhat.Value'Img);
+
            case Event.Jhat.Value is
-              when SDL.Joystick.HAT_CENTERED => Log("Handle_Events","HAT_CENTERED"); 
+              when SDL.Joystick.HAT_CENTERED => Log("Handle_Events","HAT_CENTERED");
               when SDL.Joystick.HAT_UP       => Log("Handle_Events","HAT_UP");
               when SDL.Joystick.HAT_RIGHT    => Log("Handle_Events","HAT_RIGHT");
               when SDL.Joystick.HAT_DOWN     => Log("Handle_Events","HAT_DOWN");
               when SDL.Joystick.HAT_LEFT     => Log("Handle_Events","HAT_LEFT");
               when others                    => Log("Handle_Events","others");
-           end case;                                                                        
-           
+           end case;
+
          when others => null;
            Log("Handle_Events","Event.The_Type " & Event.The_Type'Img );
       end case;
@@ -119,43 +119,43 @@ procedure Tele is
 
    -- Log("Handle_Events","Stop");
   end Handle_Events;
-  -----------------------------------------------  
-  
+  -----------------------------------------------
+
   Number_Of_Joysticks : C.Int := 0;
   Quit : Boolean := False;
   Joy_Ptr  : array (0..1) of SDL.Joystick.Joystick_Ptr := (others => SDL.Joystick.Null_Joystick_Ptr);
-  Result : C.Int := 0;
+  --Result : C.Int := 0;
 
 begin
     Log("main","Hello");
-    if SDL.Init (SDL.INIT_JOYSTICK ) < 0 then
+    if SDL.Init (SDL.INIT_JOYSTICK + SDL.INIT_VIDEO) < 0 then
        Log ("main.Init","Couldn't initialize SDL: " &
                  C.Strings.Value (SDL.Error.GetError));
        GNAT.OS_Lib.OS_Exit (1);
-    end if;  
-    
+    end if;
+
     SDL.Quit.Atexit (SDL.SDL_Quit'Access);
 
     Number_Of_Joysticks := SDL.Joystick.NumJoysticks;
     Log("main.Init","NumJoysticks:" & Number_Of_Joysticks'Img);
     for i in 0 .. Number_Of_Joysticks -1 loop
      Joy_Ptr(integer(i)) := SDL.Joystick.JoystickOpen(i);
-     Log("main.Init","opened: " & Interfaces.C.Strings.Value(SDL.Joystick.JoystickName(i))); 
-    end loop;    
-    
+     Log("main.Init","opened: " & Interfaces.C.Strings.Value(SDL.Joystick.JoystickName(i)));
+    end loop;
+
     --enable joy events
     if SDL.Joystick.JoystickEventState (SDL.Events.Enable) < 0 then
        Log ("main.Init","Couldn't enable joystick events: " &
                  C.Strings.Value (SDL.Error.GetError));
        GNAT.OS_Lib.OS_Exit (1);
     end if;
-    
-    
+
+
     loop
       Handle_Events(Quit);
       exit when Quit;
-      
+
       delay 0.1;
     end loop;
-    
+
 end Tele;
