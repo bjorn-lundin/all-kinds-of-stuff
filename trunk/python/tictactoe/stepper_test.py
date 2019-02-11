aaea5c67ee4fb92fbf29d3144b0d7dcdbdb5b6fa
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2019-02-11 19:00:06 +0000

    tests for gpio

--- trunk/python/tictactoe/stepper_test.py
+++ trunk/python/tictactoe/stepper_test.py
@@ -1,17 +1,33 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-try: 
+GPIO.setwarnings(False)
 
-    DIR = 17      # Direction GPIO Pin
+try: 
+    MAX_CNT = 2
+    DIR = 24      # Direction GPIO Pin
     STEP = 23      # Step GPIO Pin
+    ENABLE = 4
     CW = 1         # Clockwise Rotation
     CCW = 0        # Counterclockwise Rotation
     SPR = 200       # Steps per Revolution (360 / 1.8)
 
+    RELAY = 21
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(DIR, GPIO.OUT)
     GPIO.setup(STEP, GPIO.OUT)
+    GPIO.setup(ENABLE, GPIO.OUT)
+    GPIO.setup(RELAY, GPIO.OUT)
+
+
+    for x in range(10):
+            GPIO.output(RELAY, GPIO.HIGH)
+            sleep(1)
+            GPIO.output(RELAY, GPIO.LOW)
+            sleep(1)
+
+
     GPIO.output(DIR, CW)
 
 #MODE = (14, 15, 18) # Microstep Resolution GPIO Pins
@@ -24,17 +40,24 @@ try:
 #              '1/32': (1, 0, 1)}
 #
 #GPIO.output(MODE, RESOLUTION['1/32'])
+    SPR = 200
+    step_count = SPR 
+    print "stepcount", step_count
+    delay = 0.005 
+    cnt = 0
 
-    step_count = SPR * 4
-    delay = .005 / 32
+    GPIO.output(ENABLE, GPIO.LOW)
     while True:
+        cnt = cnt +1
+        GPIO.output(DIR, CW)
         for x in range(step_count):
             GPIO.output(STEP, GPIO.HIGH)
             sleep(delay)
             GPIO.output(STEP, GPIO.LOW)
             sleep(delay)
 
-        sleep(.5)
+        sleep(1)
+        print "other dir"
         GPIO.output(DIR, CCW)
 
         for x in range(step_count):
@@ -42,7 +65,9 @@ try:
             sleep(delay)
             GPIO.output(STEP, GPIO.LOW)
             sleep(delay)
-
+        if cnt >= MAX_CNT:
+            break
+        sleep(1)
 except:  
     # this catches ALL other exceptions including errors.  
     # You won't get any error messages for debugging  
@@ -51,5 +76,6 @@ except:
 
 finally:
     print "will cleanup"
-    GPIO.cleanup()
+    GPIO.output(ENABLE, GPIO.HIGH)
+#    GPIO.cleanup()
     print "did cleanup"
