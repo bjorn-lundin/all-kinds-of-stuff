3c82a20ff237bfff4f865034035cf937ea1f5891
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2019-02-23 11:28:20 +0000

    more

---
+++ trunk/ttt_robot/src/lnx_arm/gpio.adb
@@ -0,0 +1,44 @@
+
+--with Gpio.Thin ;
+
+package body Gpio is
+  
+  ---------------------------------------------------------
+  procedure Setup is
+     R : Int := 0;
+     
+    function Wiring_Pi_Setup_Gpio return Interfaces.C.Int ;
+    pragma Import(C, Wiring_Pi_Setup_Gpio, "wiringPiSetupGpio");
+     
+  begin
+    R := Wiring_Pi_Setup_Gpio;
+    if R /= 0 then
+      raise Bad_GPIO_Call with "Wiring_Pi_Setup_Gpio" & R'Img;
+    end if;
+  end Setup;
+  ---------------------------------------------------------
+  procedure Digital_Write(Pin : Interfaces.C.Int; Value : Boolean) is
+    procedure Digital_Write(Pin : Interfaces.C.Int; Value :Interfaces.C.Int);
+    pragma Import(C, Digital_Write, "digitalWrite");
+  
+  begin
+    if Value then
+      Digital_Write(Pin, HIGH);
+    else
+      Digital_Write(Pin, LOW);
+    end if;
+  end Digital_Write;
+  ------------------------------------------------------
+  procedure Digital_Read(Pin : Interfaces.C.Int; Value : out Boolean) is
+    procedure Digital_Write(Pin : Interfaces.C.Int; Value2 : out Interfaces.C.Int);
+    pragma Import(C, Digital_read, "digitalRead");
+  
+  begin
+     Digital_Write(Pin,Value2);
+     Value := Value2 = HIGH;
+  end Digital_Write;
+  ------------------------------------------------------
+  
+  
+  
+end Gpio;
