48d227392d520a8946e16d4fc974baa549702c5f
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2019-08-26 20:41:12 +0000

    new dispalyproc for pi

---
+++ trunk/sdlada-1/sdl/botresult/result.py
@@ -0,0 +1,77 @@
+"""
+ Pygame base template for opening a window
+
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+
+import sys
+import time
+from time import strftime
+import os
+import httplib
+import urllib
+import json
+
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+size = (320, 240)
+screen = pygame.display.set_mode(size)
+pygame.mouse.set_visible(0)
+
+#pygame.display.set_caption("My Game")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            done = True
+
+    # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+
+    # --- Drawing code should go here
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+#pygame.display.quit()
+pygame.quit()
+
+exit()
+
