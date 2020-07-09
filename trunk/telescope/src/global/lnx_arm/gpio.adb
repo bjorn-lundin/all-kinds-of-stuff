65c1e0f35c240785fda5d1d86d994011ffebc5f8
bnl <bnl@bab36413-0463-4170-ad3d-17c2c7d0aa3d>
2020-07-09 10:44:40 +0000

    added support many platforms

--- trunk/telescope/src/global/lnx_arm/gpio.adb
+++ trunk/telescope/src/global/lnx_arm/gpio.adb
@@ -1,44 +1,46 @@
-
 with Ada.Environment_Variables;
 
 package body Gpio is
-
+  
   ---------------------------------------------------------
   procedure Setup is
-    R : Int := 0;
-
+     R : Int := 0;
+     
     function Wiring_Pi_Setup_Gpio return Interfaces.C.Int ;
     pragma Import(C, Wiring_Pi_Setup_Gpio, "wiringPiSetupGpio");
+     
+  begin
+    -- to actually get the errorcodes ... 
+    if not Ada.Environment_Variables.Exists("WIRINGPI_CODES") then
+      Ada.Environment_Variables.Set("WIRINGPI_CODES","1"); -- to get the error codes
+    end if;    
 
-  begin --http://wiringpi.com/reference/setup/
-    -- If you want to restore the v1 behaviour, then you need to set the environment variable: WIRINGPI_CODES
-    -- to any value
-    Ada.Environment_Variables.Set("WIRINGPI_CODES","1");
     R := Wiring_Pi_Setup_Gpio;
     if R /= 0 then
-      raise Bad_Gpio_Call with "Wiring_Pi_Setup_Gpio" & R'Img;
+      raise Bad_GPIO_Call with "Wiring_Pi_Setup_Gpio" & R'Img;
     end if;
   end Setup;
   ---------------------------------------------------------
   procedure Digital_Write(Pin : Interfaces.C.Int; Value : Boolean) is
     procedure Digital_Write(Pin : Interfaces.C.Int; Value :Interfaces.C.Int);
     pragma Import(C, Digital_Write, "digitalWrite");
-
+  
   begin
     if Value then
-      Digital_Write(Pin, High);
+      Digital_Write(Pin, 1);
     else
-      Digital_Write(Pin, Low);
+      Digital_Write(Pin, 0);
     end if;
   end Digital_Write;
   ------------------------------------------------------
-
-  function Digital_Read(Pin :  Interfaces.C.Int) return Boolean is
+  function Digital_Read(Pin : Interfaces.C.Int) return Boolean is
     function Digital_Read(Pin : Interfaces.C.Int) return Interfaces.C.Int;
     pragma Import(C, Digital_Read, "digitalRead");
   begin
-    return Digital_Read(Pin) > 0 ; -- true if 1 false if 0
+     return Digital_Read(Pin) = 1 ;
   end Digital_Read;
-
-
+  ------------------------------------------------------
+  
+  
+  
 end Gpio;
