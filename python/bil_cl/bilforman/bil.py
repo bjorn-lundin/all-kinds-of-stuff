112d8cea327e247a3d83e8ecf57f3f131c72ed11
Björn Lundin <b.f.lundin@gmail.com>
2022-12-27 13:52:37 +0100

    output via rich - better looking tables

--- python/bil_cl/bilforman/bil.py
+++ python/bil_cl/bilforman/bil.py
@@ -1,4 +1,11 @@
 import csv
+from rich.table import Table
+from rich.console import Console
+#from rich.table import Column
+
+
+t=Table(title='Leasingbil verklig månadskostnad')
+c=Console()
 
 def find_tax(tbl,brt):
     with open('skatte-tabell.dat') as taxfile:
@@ -15,13 +22,29 @@ def find_tax(tbl,brt):
     return 1/0 ;# crash if not found
     
     
-print('{car:22s} {brutto:6s} {tax_with_no_car:6s} {tax_with_car:6s} {netto_utan_bil:6s} {netto_med_bil:6s} {cost:6s}'.format(
-                car='Bil', brutto='Brutto' ,tax_with_no_car='Skatt utan', tax_with_car='Skatt med',
-                netto_utan_bil='Netto utan',netto_med_bil='Netto med',cost='Kostnad' ))
+    
+t.add_column('Bil')
+t.add_column('Bruttolön', justify='right')
+t.add_column('Skatt utan bil', justify='right')
+t.add_column('Skatt med bil', justify='right')
+t.add_column('Netto utan bil', justify='right')
+t.add_column('Netto med bil', justify='right')
+t.add_column('Kostnad', justify='right')
 
-print('{car:22s} {brutto:6s} {tax_with_no_car:6s} {tax_with_car:6s} {netto_utan_bil:6s} {netto_med_bil:6s} {cost:6s}'.format(
-                car='--------------------', brutto='------' ,tax_with_no_car='----------', tax_with_car='---------',
-                netto_utan_bil='----------',netto_med_bil='---------',cost='-------' ))
+
+    
+#print('{car:22s} {brutto:6s} {tax_with_no_car:12s} {tax_with_car:6s} {netto_no_car:6s} {netto_with_car:6s} #{cost:6s}'.format(
+#                car='Bil', brutto='Brutto' ,tax_with_no_car='Skatt utan bil', tax_with_car='Skatt med bil',
+#                netto_no_car='Netto utan bil',netto_with_car='Netto med bil',cost='Kostnad' ))
+#
+#print('{car:22s} {brutto:6s} {tax_with_no_car:12s} {tax_with_car:6s} {netto_no_car:6s} {netto_with_car:6s} #{cost:6s}'.format(
+#                car='-----------------------',
+#                brutto='------' ,
+#                tax_with_no_car='-------------',
+#                tax_with_car='---------',
+#                netto_no_car='----------',
+#                netto_with_car='---------',
+#                cost='-------' ))
     
                 
 with open('input.csv') as csvfile:
@@ -37,13 +60,16 @@ with open('input.csv') as csvfile:
         income_to_find_taxes = income_after_BLA + fmv
         tax_with_no_car=find_tax(table,brutto)
         tax_with_car=find_tax(table,income_to_find_taxes)
-        netto_utan_bil = brutto - tax_with_no_car
-        netto_med_bil = brutto - bla - tax_with_car
-        cost = netto_utan_bil - netto_med_bil
+        netto_no_car = brutto - tax_with_no_car
+        netto_with_car = brutto - bla - tax_with_car
+        cost = netto_no_car - netto_with_car
+        t.add_row(car, str(brutto), str(tax_with_no_car), str(tax_with_car),
+                str(netto_no_car), str(netto_with_car), str(cost))
 
-        print('{car:22s} {brutto:6d} {tax_with_no_car:6d} {tax_with_car:10d} {netto_utan_bil:9d} {netto_med_bil:10d} {cost:8d}'.format(
-                car=car, brutto=brutto,tax_with_no_car=tax_with_no_car, tax_with_car=tax_with_car,
-                netto_utan_bil=netto_utan_bil,netto_med_bil=netto_med_bil,cost=cost ))
+#        print('{car:22s} {brutto:6d} {tax_with_no_car:12d} {tax_with_car:12d} {netto_no_car:9d} #{netto_with_car:10d} {cost:8d}'.format(
+#                car=car, brutto=brutto,tax_with_no_car=tax_with_no_car, tax_with_car=tax_with_car,
+#                netto_no_car=netto_no_car,netto_with_car=netto_with_car,cost=cost ))
 
+c.print(t)
 
 
